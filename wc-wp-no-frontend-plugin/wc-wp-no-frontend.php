2aa7442bd9baf1fc15b4a0faf74b852fc56dc7dd
Stephane Sauve <stephane.s@wiredcreation.com>
2018-09-10 16:01:23 -0400

    INIT

---
+++ wc-wp-no-frontend-plugin/wc-wp-no-frontend.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name:  WC WP No Frontend
+Plugin URI:
+Description:  Disables the frontend without blocking the REST API. (Suggested theme: WC WP No Frontend)
+Version:      1.0
+Author:       WiredCreation
+Author URI:
+License:
+License URI:
+*/
+
+defined('ABSPATH') or die('No script kiddies please!');
+
+/**
+ * Avoid a front end and ensure only the admin login can be seen
+ */
+function wc_redirect_frontend_to_login(){
+    if (!defined("REST_REQUEST") && !is_admin() && $GLOBALS['pagenow'] !== 'wp-login.php'){
+        wp_redirect(site_url('wp-admin'));
+        exit;
+    }
+}
+
+add_action("parse_request", "wc_redirect_frontend_to_login");
+
+
+
+
+//
---
+++ wc-wp-no-frontend-theme/index.php
@@ -0,0 +1,2 @@
+<?php
+echo "Oops, you should not be seeing this! Please ensure that the WC WP No Frontend plugin is active.";
